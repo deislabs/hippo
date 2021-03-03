e5b32b594659ced76c630d878f2f983fe5c90890
Matthew Fisher <matt.fisher@microsoft.com>
2021-03-03 13:18:15 -0800

    more PIDFile fiddling
    Signed-off-by: Matthew Fisher <matt.fisher@microsoft.com>

--- releases/models.py
+++ releases/models.py
@@ -95,8 +95,7 @@ class Release(UuidTimestampedModel):
         svc += 'Description=Pegasus runtime for app {}\n\n'.format(self.owner.name)
         svc += '[Service]\n'
         svc += 'Type=simple\n'
-        svc += 'ExecStart=/usr/local/bin/wagi --config {} --listen 0.0.0.0:0\n'.format(self.wagi_config_path())
-        svc += "ExecStartPost=/bin/sh -c 'umask 022; pgrep pegasus-{} > /run/pegasus-{}.pid'\n".format(self.owner.name, self.owner.name)
+        svc += "ExecStart=/bin/bash -c 'echo $$ > /run/pegasus-{}.pid; exec /usr/local/bin/wagi --config {} --listen 0.0.0.0:0\n'".format(self.owner.name, self.wagi_config_path())
         svc += 'PIDFile=/run/pegasus-{}.pid\n\n'.format(self.owner.name)
         svc += '[Install]\nWantedBy=multi-user.target\n'
         return svc
