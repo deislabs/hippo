9e88821b32bb200d9c325c5c26ae9bf0cf1dd4a5
Ivan Towlson <github@hestia.cc>
2021-06-15 12:33:31 +1200

    Provide error logs instead of exception spew on missing config

--- Hippo/Schedulers/WagiLocalJobScheduler.cs
+++ Hippo/Schedulers/WagiLocalJobScheduler.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Text;
 using Hippo.Models;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Hippo.Schedulers
 {
@@ -12,9 +14,17 @@ namespace Hippo.Schedulers
     {
         // This assumes a singleton scheduler instance!
         private readonly Dictionary<Guid, int> _wagiProcessIds = new();
+        private readonly ILogger _logger;
 
-        public WagiLocalJobScheduler(IHostApplicationLifetime lifetime)
+        private const string ENV_BINDLE = "BINDLE_SERVER_URL";
+
+        private const string ENV_WAGI = "HIPPO_WAGI_PATH";
+
+
+        public WagiLocalJobScheduler(IHostApplicationLifetime lifetime, ILogger<WagiLocalJobScheduler> logger)
         {
+            _logger = logger;
+
             lifetime.ApplicationStopping.Register(() => {
                 foreach (var processId in _wagiProcessIds)
                 {
@@ -39,19 +49,39 @@ namespace Hippo.Schedulers
             var port = c.PortID + Channel.EphemeralPortRange;
 
             var wagiProgram = WagiBinaryPath();
+            var bindleUrl = Environment.GetEnvironmentVariable(ENV_BINDLE);
+
+            if (string.IsNullOrWhiteSpace(bindleUrl))
+            {
+                _logger.LogError($"Bindle server URL not specified: set {ENV_BINDLE}");
+                return;
+            }
+
             var psi = new ProcessStartInfo
             {
                 FileName = wagiProgram,
-                Arguments = $"-b {c.Application.StorageId}/{c.ActiveRevision.RevisionNumber} --bindle-server {Environment.GetEnvironmentVariable("BINDLE_SERVER_URL")} --default-host localhost:{port} -l 127.0.0.1:{port}",
+                Arguments = $"-b {c.Application.StorageId}/{c.ActiveRevision.RevisionNumber} --bindle-server {bindleUrl} --default-host localhost:{port} -l 127.0.0.1:{port}",
             };
-            psi.Environment["BINDLE_SERVER_URL"] = Environment.GetEnvironmentVariable("BINDLE_SERVER_URL");
+            psi.Environment["BINDLE_SERVER_URL"] = bindleUrl;
             // TODO: drive this from outside
             psi.Environment["RUST_LOG"] = "warn,wagi=trace";
 
-            using (var process = Process.Start(psi))
+            try
             {
-                process.Exited += (s, e) => _wagiProcessIds.Remove(c.Id);
-                _wagiProcessIds[c.Id] = process.Id;
+                using (var process = Process.Start(psi))
+                {
+                    process.Exited += (s, e) => _wagiProcessIds.Remove(c.Id);
+                    _wagiProcessIds[c.Id] = process.Id;
+                }
+            }
+            catch (Win32Exception e)  // yes, even on Linux
+            {
+                if (e.Message.Contains("No such file or directory"))
+                {
+                    _logger.LogError($"Program '{wagiProgram}' not found: check system path or set {ENV_WAGI}");
+                    return;
+                }
+                throw;
             }
         }
 
@@ -66,7 +96,7 @@ namespace Hippo.Schedulers
 
         private static string WagiBinaryPath()
         {
-            return Environment.GetEnvironmentVariable("HIPPO_WAGI_PATH") ??
+            return Environment.GetEnvironmentVariable(ENV_WAGI) ??
                 (OperatingSystem.IsWindows() ? "wagi.exe" : "wagi");
         }
 
