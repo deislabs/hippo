7ea88b3efc61851c7ee2833db7df7669adf58ed0
Simon Davies <simongdavies@users.noreply.github.com>
2021-06-16 19:48:48 +0100

    Add App API

---
+++ Hippo/Messages/ApplicationMessage.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Hippo.Messages
+{
+    public class ApplicationMessage
+    {
+        /// <summary>
+        /// The name of the ApplicationController
+        /// </summary>
+        /// <example>My Excellent Application</example>
+        [Required]
+        public string ApplicationName { get; set; }
+
+        /// <summary>
+        /// This is the ID in Bindle or whatever storage backend is used.It gets composed
+        /// with a revision ID to get a Bindle ID.
+        ///
+        /// For example, the Weather application might have the StorageId contoso/weather.
+        /// Revision 1.4.0 of the Weather application would then have the bindle id
+        /// contoso/weather/1.4.0
+        /// </summary>
+        /// <example>contoso/weather</example>
+        [Required]
+        public string StorageId { get; set; }
+    }
+}
---
+++ Hippo/Messages/CreateApplicationResponse.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Hippo.Logging;
+
+namespace Hippo.Messages
+{
+    /// <summary>
+    /// Response body for a new Hippo Application API.
+    /// </summary>
+    public class CreateApplicationResponse: ApplicationMessage, ITraceable
+    {
+        /// <summary>
+        /// The name GUID of the new Application
+        /// </summary>
+        /// <example>4208d635-7618-4150-b8a8-bc3205e70e32</example>
+        [Required]
+        public Guid ApplicationGUID { get; set; }
+        /// <summary>
+        /// ITraceable.FormatTrace implementation.
+        /// </summary>
+        /// <returns>Trace string</returns>
+        public string FormatTrace()
+        => $"{GetType().Name}[ApplicationName={ApplicationName}, ApplicationGUID={ApplicationGUID}], StorageId={StorageId}]";
+    }
+}
