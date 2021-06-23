8925e8f1e2ff8da25fac46564a5800f2bbb79fde
Simon Davies <simongdavies@users.noreply.github.com>
2021-06-23 21:04:28 +0100

    fix more line endings

--- Hippo.Tests/ApiControllers/MockTokenIssuer.cs
+++ Hippo.Tests/ApiControllers/MockTokenIssuer.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
--- Hippo.Tests/Stubs/FakeCurrentUser.cs
+++ Hippo.Tests/Stubs/FakeCurrentUser.cs
@@ -1,4 +1,4 @@
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Security.Principal;
 using System.Threading;
 using Hippo.Controllers;
