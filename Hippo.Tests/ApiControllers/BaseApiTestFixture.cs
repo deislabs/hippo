9a0bf404f4ec2618d538da3d8c24b37799bdba23
Simon Davies <simongdavies@users.noreply.github.com>
2021-06-24 21:56:25 +0100

    Adds additonal API Tests

---
+++ Hippo.Tests/ApiControllers/BaseApiTestFixture.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Security.Claims;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+using Hippo.ApiControllers;
+using Hippo.Messages;
+using Hippo.Models;
+using Hippo.Repositories;
+using Hippo.Tasks;
+using Hippo.Tests.Stubs;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace Hippo.Tests.ApiControllers
+{
+    public class BaseApiTestFixture
+    {
+        public MockTokenIssuer TokenIssuer { get; private set; }
+
+        public TestServer Server { get; protected set; }
+
+        public DataContext Context { get; protected set; }
+
+        public Guid AppId { get; private set; }
+
+        public Application Application { get; private set; }
+
+        public Account User { get; private set; }
+
+        public BaseApiTestFixture(string testDatabaseName)
+        {
+            AppId = Guid.NewGuid();
+            User = new()
+            {
+                UserName = "user",
+                Id = "2",
+            };
+            Application = new()
+            {
+                Id = AppId,
+                Name = "Test Application",
+                StorageId = "hippo/test",
+                Owner = User
+            };
+            Context = new DataContext(
+              new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: testDatabaseName)
+                .Options);
+            Context.Database.EnsureDeleted();
+            Context.Database.EnsureCreated();
+            Context.Applications.Add(Application);
+            Context.Users.Add(User);
+            Context.SaveChanges();
+            TokenIssuer = new MockTokenIssuer();
+        }
+    }
+}
--- Hippo.Tests/ApiControllers/TestStartup.cs
+++ Hippo.Tests/ApiControllers/TestStartup.cs
@@ -2,21 +2,34 @@ using System;
 using System.Reflection;
 using Hippo.Models;
 using Hippo.Repositories;
+using Hippo.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Xunit;
 
 namespace Hippo.Tests.ApiControllers
 {
     public class TestStartup
     {
-        private readonly MockTokenIssuer TokenIssuer;
-        public TestStartup(MockTokenIssuer tokenIssuer)
+        private readonly MockTokenIssuer _tokenIssuer;
+        private readonly ITaskQueue<ChannelReference> _taskQueue;
+        private readonly string _testDatabaseName;
+
+        public TestStartup(MockTokenIssuer tokenIssuer, string testDatabaseName)
         {
-            TokenIssuer = tokenIssuer;
+            Assert.NotNull(tokenIssuer);
+            _tokenIssuer = tokenIssuer;
+            Assert.False(string.IsNullOrEmpty(testDatabaseName));
+            _testDatabaseName = testDatabaseName;
+        }
+
+        public TestStartup(MockTokenIssuer tokenIssuer, string testDatabaseName, ITaskQueue<ChannelReference> taskQueue) : this(tokenIssuer, testDatabaseName)
+        {
+            _taskQueue = taskQueue;
         }
         public void ConfigureServices(IServiceCollection services)
         {
@@ -25,18 +38,18 @@ namespace Hippo.Tests.ApiControllers
                 cfg.User.RequireUniqueEmail = true;
             }).AddEntityFrameworkStores<DataContext>();
             services.AddControllers().AddApplicationPart(Assembly.Load("Hippo")).AddControllersAsServices();
+
             services.AddAuthentication().AddJwtBearer(
-              cfg =>
-              cfg.TokenValidationParameters = new TokenValidationParameters
-              {
-                  ValidateIssuer = true,
-                  ValidateAudience = true,
-                  ValidateLifetime = true,
-                  ValidateIssuerSigningKey = true,
-                  ValidIssuer = TokenIssuer.Issuer,
-                  ValidAudience = TokenIssuer.Audience,
-                  IssuerSigningKey = TokenIssuer.SecurityKey
-              }
+                cfg => cfg.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = _tokenIssuer.Issuer,
+                    ValidAudience = _tokenIssuer.Audience,
+                    IssuerSigningKey = _tokenIssuer.SecurityKey
+                }
             );
 
 
@@ -49,15 +62,15 @@ namespace Hippo.Tests.ApiControllers
 
             services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
             services.AddDbContext<DataContext>(
-                    options =>
-                    options.UseInMemoryDatabase("Hippo")
-                );
-
-
+                options =>
+                options.UseInMemoryDatabase(_testDatabaseName)
+            );
             services.AddScoped<ICurrentUser, ActionContextCurrentUser>();
             services.AddScoped<IUnitOfWork, DbUnitOfWork>();
-
-            services.AddTransient<DataSeeder>();
+            if (_taskQueue != null)
+            {
+                services.AddSingleton<ITaskQueue<ChannelReference>>(_taskQueue);
+            }
 
             services.AddRouting(options => options.LowercaseUrls = true);
             services.AddMvc();
