95a7ec1ef039d7ff81cd27c6a33910543ecd8178
itowlson <itowlson@microsoft.com>
2021-06-28 15:57:40 +1200

    Prerelease channels (#89)

---
+++ Hippo.Tests/Rules/RevisionRangeRuleTests.cs
@@ -0,0 +1,103 @@
+using System.Collections.Generic;
+using System.Linq;
+using Hippo.Models;
+using Hippo.Rules;
+using Xunit;
+
+namespace Hippo.Tests.Rules
+{
+    public class RevisionRangeRuleTests
+    {
+        // Scenarios:
+        // * Plain ol' NPM production releases: ^1.1, ~1.2.3, etc. - uninteresting from a testing
+        //   POV because the versioning library does them for us
+        // * Most recent prerelease by user: ivan, canary, etc.
+        //   - This addresses both the dev inner loop and the CI "trunk tracking" scenario
+        // * Most recent prerelease *within a version*. This addresses the scenario where a
+        //   developer has hotfix or backport builds against multiple versions e.g. I am
+        //   fixing a UI issue in 1.4.1 and a database error in 1.6.0.
+        //   - It is reasonable to assume that these channels are likely to be relatively
+        //     shortlived, and that the developer will know the *full* version they are
+        //     building from. So we need to support 1.4.1-ivan-latest but not e.g.
+        //     ^1.4-ivan-latest.
+        //   - This *might* fall foul of rebase situations. Alice is working on a hotfix
+        //     against 1.4.1. But so is Bob. Bob's hotfix is released as 1.4.2. Now Alice
+        //     has to rebase and update her channel definition. BO-RING!
+        // * Collaboration on a feature. If Alice and Bob are both working on a spongiforms
+        //   feature for 1.4.0, we should be able to set up a channel for the 1.4.0-spongiforms
+        //   latest prerelease.  (Alice and Bob may still want individual channels though.)
+        //   - This is also relevant for "canary deployment of a feature branch."
+
+        private static readonly List<Revision> _revisions = new string[] {
+            "1.1.0",
+            "1.1.1",
+            "1.1.2",
+            "1.1.3",
+            "1.1.3-alice-2021.01.01.01.01.01.111",
+            "1.1.3-alice-2021.04.04.04.04.04.444",
+            "1.1.3-bob-2021.02.02.02.02.02.222",
+            "1.1.3-spongiforms-alice-2021.03.03.03.03.03.333",
+            "1.1.3-spongiforms-alice-2021.02.02.02.02.02.222",
+            "1.1.3-spongiforms-bob-2021.02.02.02.02.02.222",
+            "1.1.3-spongiforms-2021.05.05.05.05.05.555",
+            "1.1.3-spongiforms-2021.04.04.04.04.04.444",
+            "1.1.4-alice-2021.01.02.03.04.05.678",
+            "1.2.0",
+            "1.2.1",
+            "2.0.0",
+            "2.1.1",
+            "2.1.1-canary-2021.04.04.04.04.04.444",
+            "2.1.2-canary-2021.03.03.03.03.03.333",
+        }.Select(r => new Revision { RevisionNumber = r }).ToList();
+
+        private static void AssertMatchResult(string expected, RevisionRangeRule rule)
+        {
+            Assert.Equal(expected, rule.Match(_revisions)?.RevisionNumber);
+        }
+
+        [Fact]
+        public void NpmPatternsIgnorePrereleases()
+        {
+            AssertMatchResult("1.2.0", RevisionRangeRule.Parse("1.2.0"));
+            AssertMatchResult("1.1.3", RevisionRangeRule.Parse("~1.1"));
+            AssertMatchResult("2.1.1", RevisionRangeRule.Parse("~2"));
+            AssertMatchResult("1.2.1", RevisionRangeRule.Parse("^1.1"));
+        }
+
+        [Fact]
+        public void SupportsMostRecentReleaseByUser()
+        {
+            AssertMatchResult("1.1.3-alice-2021.04.04.04.04.04.444", RevisionRangeRule.Parse("P:*-alice-*"));
+            AssertMatchResult("1.1.3-bob-2021.02.02.02.02.02.222", RevisionRangeRule.Parse("P:*-bob-*"));
+            AssertMatchResult("2.1.1-canary-2021.04.04.04.04.04.444", RevisionRangeRule.Parse("P:*-canary-*"));
+        }
+
+        [Fact]
+        public void SupportsMostRecentReleaseByUserWithinAVersion()
+        {
+            AssertMatchResult("1.1.3-alice-2021.04.04.04.04.04.444", RevisionRangeRule.Parse("P:1.1.3-alice-*"));
+            AssertMatchResult("1.1.4-alice-2021.01.02.03.04.05.678", RevisionRangeRule.Parse("P:1.1.4-alice-*"));
+        }
+
+        // [Fact]
+        // public void SupportsMostRecentReleaseByUserWithinVersionRange()
+        // {
+        //     // TODO: may not need this
+        //     // TODO: define behaviour - should higher version or more recent timestamp take precedence?
+        //     AssertMatchResult("1.1.4-alice-2021.01.02.03.04.05.678", RevisionRangeRule.Parse("P:~1.1-alice-*"));
+        // }
+
+        [Fact]
+        public void SupportsLongRunningFeatureBranches()
+        {
+            AssertMatchResult("1.1.3-spongiforms-2021.05.05.05.05.05.555", RevisionRangeRule.Parse("P:*-spongiforms-*"));
+        }
+
+        [Fact]
+        public void SupportsContributorsInLongRunningFeatureBranches()
+        {
+            AssertMatchResult("1.1.3-spongiforms-alice-2021.03.03.03.03.03.333", RevisionRangeRule.Parse("P:*-spongiforms-alice-*"));
+            AssertMatchResult("1.1.3-spongiforms-bob-2021.02.02.02.02.02.222", RevisionRangeRule.Parse("P:*-spongiforms-bob-*"));
+        }
+    }
+}
--- Hippo/Rules/RevisionRangeRule.cs
+++ Hippo/Rules/RevisionRangeRule.cs
@@ -1,34 +1,124 @@
+#nullable enable
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Hippo.Models;
 using SemVer;
+using Range = SemVer.Range;
 
 namespace Hippo.Rules
 {
-    public class RevisionRangeRule
+    public abstract class RevisionRangeRule
     {
-        private readonly Range _range;
-
-        public static RevisionRangeRule Parse(string rule)
+        public static RevisionRangeRule Parse(string? rule)
         {
-            return new RevisionRangeRule(new Range(rule));
-        }
-
-        public RevisionRangeRule(Range range)
-        {
-            _range = range;
-        }
-
-        public Revision Match(ICollection<Revision> candidates)
-        {
-            var candidatesByVersion = candidates.ToDictionary(c => c.RevisionNumber, c => c);
-            var maxSatisfying = _range.MaxSatisfying(candidatesByVersion.Keys);
-
-            if (maxSatisfying == null)
+            if (rule is null)
             {
-                return null;
+                throw new ArgumentNullException(nameof(rule));
             }
-            return candidatesByVersion[maxSatisfying];
+
+            if (rule.StartsWith("P:", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return new PrereleaseRevisionRangeRule(rule);
+            }
+            return new NpmRevisionRangeRule(rule);
+        }
+
+        public abstract Revision? Match(ICollection<Revision> candidates);
+
+        private class NpmRevisionRangeRule : RevisionRangeRule
+        {
+            private readonly Range _range;
+
+            public NpmRevisionRangeRule(string rule)
+            {
+                _range = new Range(rule);
+            }
+
+            public override Revision? Match(ICollection<Revision> candidates)
+            {
+                var candidatesByVersion = candidates.ToDictionary(c => c.RevisionNumber, c => c);
+                var maxSatisfying = _range.MaxSatisfying(candidatesByVersion.Keys);
+
+                if (maxSatisfying == null)
+                {
+                    return null;
+                }
+                return candidatesByVersion[maxSatisfying];
+            }
+        }
+
+        private class PrereleaseRevisionRangeRule : RevisionRangeRule
+        {
+            private readonly Range _versionRange;
+            private readonly string _prereleasePrefix;
+
+            public PrereleaseRevisionRangeRule(string rule)
+            {
+                // We expect this to be of the form P:(*|npmpat)-<name>-*
+
+                // TODO: this is a bit horrid - drop this bit or implement globbing
+                if (!rule.EndsWith("-*", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    throw new ArgumentException("patterns after name not supported", nameof(rule));
+                }
+
+                var body = rule.Substring(2, rule.Length - 4);
+
+                var splitAt = body.IndexOf('-');
+                var versionPattern = body.Substring(0, splitAt);
+                var prereleasePattern = body.Substring(splitAt + 1);
+
+                if (versionPattern.Length == 0 || prereleasePattern.Length == 0)
+                {
+                    throw new ArgumentException("rule must have something before and after the hyphen", nameof(rule));
+                }
+
+                _versionRange = new Range(versionPattern);
+                _prereleasePrefix = prereleasePattern;
+            }
+
+            public override Revision? Match(ICollection<Revision> candidates)
+            {
+                var candidatesByVersion =
+                    from c in candidates
+                    from pr in ParsePrerelease(c.RevisionNumber)
+                    where Matches(pr)
+                    orderby pr.Timestamp descending
+                    select c;
+
+                return candidatesByVersion.FirstOrDefault();
+            }
+
+            private IEnumerable<(string Version, string User, string Timestamp)> ParsePrerelease(string revisionNumber)
+            {
+                var prereleaseParse = revisionNumber.IndexOf('-');
+                if (prereleaseParse <= 0)
+                {
+                    yield break;
+                }
+
+                var version = revisionNumber.Substring(0, prereleaseParse);
+                var prerelease = revisionNumber.Substring(prereleaseParse + 1);
+
+                var tsParse = prerelease.LastIndexOf('-');
+                if (tsParse <= 0)
+                {
+                    yield break;
+                }
+
+                var user = prerelease.Substring(0, tsParse);
+                var timestamp = prerelease.Substring(tsParse + 1);
+
+                yield return (version, user, timestamp);
+            }
+
+            private bool Matches((string Version, string User, string _) revisionNumber)
+            {
+                return revisionNumber.User == _prereleasePrefix &&
+                    _versionRange.IsSatisfied(revisionNumber.Version);
+            }
         }
     }
 }
