d278319854c3569f18934bb4ccf7aac85fbba244
itowlson <itowlson@microsoft.com>
2021-07-06 15:04:19 +1200

    Show revisions list in app details (#114)

--- Hippo/Models/Revision.cs
+++ Hippo/Models/Revision.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Text;
 
 namespace Hippo.Models
@@ -11,5 +13,17 @@ namespace Hippo.Models
         // to get the bindle ID.  E.g. this might be "1.4.0" or "1.1.5-prerelease2".
         [Required]
         public string RevisionNumber { get; set; }
+
+        public IEnumerable<Channel> ActiveOn() =>
+            Application.Channels.Where(c => c.ActiveRevision?.RevisionNumber == RevisionNumber);
+
+        public string OrderKey()
+        {
+            if (SemVer.Version.TryParse(RevisionNumber, out var version))
+            {
+                return $"{version.Major:D9}{version.Minor:D9}{version.Patch:D9}{RevisionNumber}";
+            }
+            return RevisionNumber;
+        }
     }
 }
