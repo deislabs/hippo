edd098f39c546c6647fd3189a994eae4ccc3b1b3
itowlson <itowlson@microsoft.com>
2021-07-23 08:05:10 +1200

    Make migrations generate correct SQLite timestamp function (#172)

---
+++ Hippo/Migrations/Sqlite/20210722051132_Timestamp.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hippo.Migrations.Sqlite
+{
+    public partial class Timestamp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Revisions",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Revisions",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Keys",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Keys",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "EventLogEntries",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "EventLogEntries",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "EnvironmentVariables",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "EnvironmentVariables",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Domains",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Domains",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Configuration",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Configuration",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Channels",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Channels",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Applications",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Applications",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "datetime('now')",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "now()");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Revisions",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Revisions",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Keys",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Keys",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "EventLogEntries",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "EventLogEntries",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "EnvironmentVariables",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "EnvironmentVariables",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Domains",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Domains",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Configuration",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Configuration",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Channels",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Channels",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Applications",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Applications",
+                type: "TEXT",
+                nullable: false,
+                defaultValueSql: "now()",
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT",
+                oldDefaultValueSql: "datetime('now')");
+        }
+    }
+}
