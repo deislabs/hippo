e4f8df8fea0d7ecbab71061941b04698bd94f729
Matthew Fisher <matt.fisher@fermyon.com>
2021-12-17 11:34:15 -0800

    require unique domain name
    Signed-off-by: Matthew Fisher <matt.fisher@fermyon.com>

--- src/Application/Channels/Commands/UpdateChannelCommandValidator.cs
+++ src/Application/Channels/Commands/UpdateChannelCommandValidator.cs
@@ -1,5 +1,7 @@
 using System.Text.RegularExpressions;
 using FluentValidation;
+using Hippo.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hippo.Application.Channels.Commands;
 
@@ -8,9 +10,12 @@ public class UpdateChannelCommandValidator : AbstractValidator<UpdateChannelComm
     private readonly Regex validName = new Regex("^[a-zA-Z0-9-_]*$");
 
     private readonly Regex validDomainName = new Regex("^((?!-)[A-Za-z0-9-]{1, 63}(?<!-)\\.)+[A-Za-z]{2, 6}$");
+    private readonly IApplicationDbContext _context;
 
-    public UpdateChannelCommandValidator()
+    public UpdateChannelCommandValidator(IApplicationDbContext context)
     {
+        _context = context;
+
         RuleFor(v => v.Name)
             .NotEmpty().WithMessage("Name is required.")
             .MaximumLength(64)
@@ -18,10 +23,14 @@ public class UpdateChannelCommandValidator : AbstractValidator<UpdateChannelComm
 
         RuleFor(v => v.Domain)
             .NotEmpty().WithMessage("Domain is required.")
-            .Matches(validDomainName);
-
-        // TODO: do we want to ensure no domains collide with each other?
+            .Matches(validDomainName)
+            .MustAsync(BeUniqueDomainName).WithMessage("The specified domain already exists.");
 
         // TODO: validate RangeRule
+    }
+
+    public async Task<bool> BeUniqueDomainName(string domain, CancellationToken cancellationToken)
+    {
+        return await _context.Channels.AllAsync(a => a.Domain != domain, cancellationToken);
     }
 }
