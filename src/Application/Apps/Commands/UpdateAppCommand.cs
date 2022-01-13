01a0b5ebeefee5a7079b338d71aff496c423685c
Matthew Fisher <matt.fisher@fermyon.com>
2022-01-12 17:39:10 -0800

    stop all channels when storage ID has been updated
    Signed-off-by: Matthew Fisher <matt.fisher@fermyon.com>

--- src/Application/Apps/Commands/UpdateAppCommand.cs
+++ src/Application/Apps/Commands/UpdateAppCommand.cs
@@ -18,9 +18,12 @@ public class UpdateAppCommandHandler : IRequestHandler<UpdateAppCommand>
 {
     private readonly IApplicationDbContext _context;
 
-    public UpdateAppCommandHandler(IApplicationDbContext context)
+    private readonly IJobScheduler _jobScheduler;
+
+    public UpdateAppCommandHandler(IApplicationDbContext context, IJobScheduler jobScheduler)
     {
         _context = context;
+        _jobScheduler = jobScheduler;
     }
 
     public async Task<Unit> Handle(UpdateAppCommand request, CancellationToken cancellationToken)
@@ -31,6 +34,17 @@ public class UpdateAppCommandHandler : IRequestHandler<UpdateAppCommand>
         if (entity == null)
         {
             throw new NotFoundException(nameof(App), request.Id);
+        }
+
+        // if the user changes the bindle storage id, ALL channels will stop working until the user registers new revisions that satisfy the channels' rules.
+        //
+        // TODO: how do we want to handle channels that requested ChannelRevisionSelectionStrategy.UseSpecifiedRevision?
+        if (entity.StorageId != request.StorageId)
+        {
+            foreach (var channel in entity.Channels)
+            {
+                _jobScheduler.Stop(channel);
+            }
         }
 
         entity.Name = request.Name;
