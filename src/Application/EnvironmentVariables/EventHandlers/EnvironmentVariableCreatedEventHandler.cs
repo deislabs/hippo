e9cfa02dc77a475c284e2e40cc5aebe8ac5bb082
Matthew Fisher <matt.fisher@fermyon.com>
2022-04-07 11:08:42 -0700

    Restart job when adding environment variable
    Signed-off-by: Matthew Fisher <matt.fisher@fermyon.com>

--- src/Application/EnvironmentVariables/EventHandlers/EnvironmentVariableCreatedEventHandler.cs
+++ src/Application/EnvironmentVariables/EventHandlers/EnvironmentVariableCreatedEventHandler.cs
@@ -1,3 +1,4 @@
+using Hippo.Application.Common.Interfaces;
 using Hippo.Application.Common.Models;
 using Hippo.Core.Entities;
 using Hippo.Core.Events;
@@ -9,17 +10,35 @@ namespace Hippo.Application.EnvironmentVariables.EventHandlers;
 public class EnvironmentVariableCreatedEventHandler : INotificationHandler<DomainEventNotification<CreatedEvent<EnvironmentVariable>>>
 {
     private readonly ILogger<EnvironmentVariableCreatedEventHandler> _logger;
+    private readonly IJobFactory _jobFactory;
 
-    public EnvironmentVariableCreatedEventHandler(ILogger<EnvironmentVariableCreatedEventHandler> logger)
+    public EnvironmentVariableCreatedEventHandler(ILogger<EnvironmentVariableCreatedEventHandler> logger, IJobFactory jobFactory)
     {
         _logger = logger;
+        _jobFactory = jobFactory;
     }
 
     public Task Handle(DomainEventNotification<CreatedEvent<EnvironmentVariable>> notification, CancellationToken cancellationToken)
     {
         var domainEvent = notification.DomainEvent;
+        var channel = domainEvent.Entity.Channel;
 
         _logger.LogInformation("Hippo Domain Event: {DomainEvent}", domainEvent.GetType().Name);
+
+        if (channel.ActiveRevision is not null)
+        {
+            _logger.LogInformation($"{channel.App.Name}: Restarting channel {channel.Name} at revision {channel.ActiveRevision.RevisionNumber}");
+            var envvars = channel.EnvironmentVariables.ToDictionary(
+                e => e.Key!,
+                e => e.Value!
+            );
+            _jobFactory.Start(channel.Id, $"{channel.App.StorageId}/{channel.ActiveRevision.RevisionNumber}", envvars, channel.Domain);
+            _logger.LogInformation($"Started {channel.App.Name} Channel {channel.Name} at revision {channel.ActiveRevision.RevisionNumber}");
+        }
+        else
+        {
+            _logger.LogInformation($"Not restarting {channel.App.Name} Channel {channel.Name}: no active revision");
+        }
 
         return Task.CompletedTask;
     }
--- src/Application/EnvironmentVariables/EventHandlers/EnvironmentVariableDeletedEventHandler.cs
+++ src/Application/EnvironmentVariables/EventHandlers/EnvironmentVariableDeletedEventHandler.cs
@@ -1,3 +1,4 @@
+using Hippo.Application.Common.Interfaces;
 using Hippo.Application.Common.Models;
 using Hippo.Core.Entities;
 using Hippo.Core.Events;
@@ -10,16 +11,35 @@ public class EnvironmentVariableDeletedEventHandler : INotificationHandler<Domai
 {
     private readonly ILogger<EnvironmentVariableDeletedEventHandler> _logger;
 
-    public EnvironmentVariableDeletedEventHandler(ILogger<EnvironmentVariableDeletedEventHandler> logger)
+    private readonly IJobFactory _jobFactory;
+
+    public EnvironmentVariableDeletedEventHandler(ILogger<EnvironmentVariableDeletedEventHandler> logger, IJobFactory jobFactory)
     {
         _logger = logger;
+        _jobFactory = jobFactory;
     }
 
     public Task Handle(DomainEventNotification<DeletedEvent<EnvironmentVariable>> notification, CancellationToken cancellationToken)
     {
         var domainEvent = notification.DomainEvent;
+        var channel = domainEvent.Entity.Channel;
 
         _logger.LogInformation("Hippo Domain Event: {DomainEvent}", domainEvent.GetType().Name);
+
+        if (channel.ActiveRevision is not null)
+        {
+            _logger.LogInformation($"{channel.App.Name}: Restarting channel {channel.Name} at revision {channel.ActiveRevision.RevisionNumber}");
+            var envvars = channel.EnvironmentVariables.ToDictionary(
+                e => e.Key!,
+                e => e.Value!
+            );
+            _jobFactory.Start(channel.Id, $"{channel.App.StorageId}/{channel.ActiveRevision.RevisionNumber}", envvars, channel.Domain);
+            _logger.LogInformation($"Started {channel.App.Name} Channel {channel.Name} at revision {channel.ActiveRevision.RevisionNumber}");
+        }
+        else
+        {
+            _logger.LogInformation($"Not restarting {channel.App.Name} Channel {channel.Name}: no active revision");
+        }
 
         return Task.CompletedTask;
     }
