b524f7979ac34380ed1a7b96593db2e1913460f0
Robert Gogete <gogeterobert@yahoo.com>
2022-04-26 14:21:04 +0300

    added bindle service to dependency injection and instantiated the BindleClient

--- src/Infrastructure/Services/BindleService.cs
+++ src/Infrastructure/Services/BindleService.cs
@@ -1,7 +1,14 @@
+using Deislabs.Bindle;
 using Hippo.Application.Common.Interfaces;
 
 namespace Hippo.Infrastructure.Services;
 
 public class BindleService : IBindleService
 {
+    private BindleClient _client { get; set; }
+
+    public BindleService()
+    {
+        _client = new BindleClient(configuration.GetValue<string>("Bindle:Url", "http://127.0.0.1:8080/v1"));
+    }
 }
