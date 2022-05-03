fe05c62e1be3e6bc273bd54b9e17ba5fe1ad6d2a
Stefan Nedelcu <nedelcu.stefan17@yahoo.com>
2022-05-03 22:05:29 +0300

    Implemented mock method for revisions and fixed tests typo

--- tests/Hippo.FunctionalTests/FakeBindleService.cs
+++ tests/Hippo.FunctionalTests/FakeBindleService.cs
@@ -1,12 +1,13 @@
 ﻿using Hippo.Application.Common.Interfaces;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Hippo.FunctionalTests;
 
 internal class FakeBindleService : IBindleService
 {
+    public async Task<IEnumerable<string>> GetBindleRevisionNumbers(string bindleId)
+    {
+        return await Task.FromResult(new List<string> { "1.0.0" });
+    }
 }
