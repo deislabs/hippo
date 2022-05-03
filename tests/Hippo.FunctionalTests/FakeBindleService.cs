7d7caedebcdb68c56cfadf8814e878c0bf872914
Gogete Robert <gogeterobert@yahoo.com>
2022-05-03 18:31:28 +0300

    Add fake bindle service (#710)
    * Added a fake bindle service for when running tests
    
    * Updated namespace declaration
    
    Co-authored-by: Stefan Nedelcu <nedelcu.stefan17@yahoo.com>
    Co-authored-by: Stefan Nedelcu <60442811+StefanNedelcu@users.noreply.github.com>

---
+++ tests/Hippo.FunctionalTests/FakeBindleService.cs
@@ -0,0 +1,12 @@
+﻿using Hippo.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hippo.FunctionalTests;
+
+internal class FakeBindleService : IBindleService
+{
+}
