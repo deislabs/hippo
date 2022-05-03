8e18ad69f6a549f5a62fd5e85529a6530a51ef3a
Stefan Nedelcu <nedelcu.stefan17@yahoo.com>
2022-05-03 12:22:21 +0300

    GetBindleRevisionNumbers should not return null strings

--- src/Application/Common/Interfaces/IBindleService.cs
+++ src/Application/Common/Interfaces/IBindleService.cs
@@ -2,5 +2,5 @@ namespace Hippo.Application.Common.Interfaces;
 
 public interface IBindleService
 {
-    public Task<IEnumerable<string?>> GetBindleRevisionNumbers(string bindleId);
+    public Task<IEnumerable<string>> GetBindleRevisionNumbers(string bindleId);
 }
--- src/Infrastructure/Services/BindleService.cs
+++ src/Infrastructure/Services/BindleService.cs
@@ -10,16 +10,20 @@ public class BindleService : IBindleService
 
     public BindleService(IConfiguration configuration)
     {
-        var bindleUrl = configuration.GetValue<string>("Bindle:Url", "http://127.0.0.1:8080/v1");
+        var bindleUrl = configuration.GetValue("Bindle:Url", "http://127.0.0.1:8080/v1");
         _client = new BindleClient(bindleUrl);
     }
 
-    public async Task<IEnumerable<string?>> GetBindleRevisionNumbers(string bindleId)
+    public async Task<IEnumerable<string>> GetBindleRevisionNumbers(string bindleId)
     {
-        return (await _client.QueryInvoices(bindleId))
-            .Invoices
-            .Where(i => i.Bindle.Name == bindleId)
-            .Select(i => i.Bindle.Version)
+        var matches = await _client.QueryInvoices(bindleId);
+        if (matches.Total == 0)
+            return new List<string>();
+
+        return matches.Invoices
+            .Where(i => i.Bindle is not null)
+            .Where(i => i.Bindle?.Name == bindleId)
+            .Select(i => new string(i.Bindle?.Version))
             .ToList();
     }
 }
