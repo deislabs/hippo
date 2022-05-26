e63cb55af25acef64bc3283ee783d475f9e16dec
Stefan Nedelcu <nedelcu.stefan17@yahoo.com>
2022-05-26 15:54:16 +0300

    Add migration for redis component elements

---
+++ src/Infrastructure/Data/Migrations/Postgresql/20220526124417_AddRedisComponentElements.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hippo.Infrastructure.Data.Migrations.Postgresql
+{
+    public partial class AddRedisComponentElements : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Route",
+                table: "RevisionComponents",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Channel",
+                table: "RevisionComponents",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "RevisionComponents",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Channel",
+                table: "RevisionComponents");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "RevisionComponents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Route",
+                table: "RevisionComponents",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
---
+++ src/Infrastructure/Data/Migrations/Sqlite/20220526125320_AddRedisComponentElements.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hippo.Infrastructure.Data.Migrations.Sqlite
+{
+    public partial class AddRedisComponentElements : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Route",
+                table: "RevisionComponents",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Channel",
+                table: "RevisionComponents",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Type",
+                table: "RevisionComponents",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Channel",
+                table: "RevisionComponents");
+
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "RevisionComponents");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Route",
+                table: "RevisionComponents",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
