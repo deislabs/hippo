3e589a430d7f4b6afd15fcd01c1e5b89a5247c15
Stefan Nedelcu <60442811+StefanNedelcu@users.noreply.github.com>
2022-06-09 00:24:35 +0300

    Feature / channel overview page (#832)
    * Added a part of the UI
    
    * Channel overview component - frontend
    
    * Updated regex of updating channel validator to match the one when creating, finished the rest of the UI
    
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>
    
    * Ignoring the current updated item when checking for domain uniqueness
    
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>
    
    * Missed commented code
    
    Signed-off-by: Robert Gogete <gogeterobert@yahoo.com>
    
    * Switch to the current api endpoint
    
    Co-authored-by: Robert Gogete <gogeterobert@yahoo.com>

---
+++ src/Web/ClientApp/src/app/components/channel/overview/overview.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ChannelService, RevisionDto } from 'src/app/core/api/v1';
+import { ComponentTypes } from 'src/app/_helpers/constants';
+import { faCheckCircle, faTimesCircle, faNetworkWired } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'channel-overview',
+  templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.css']
+})
+export class OverviewComponent implements OnInit {
+  @Input() channelId = '';
+  activeRevision!: RevisionDto | undefined;
+  icons = { faCheckCircle, faTimesCircle, faNetworkWired };
+  types = ComponentTypes;
+
+  constructor(private readonly channelService: ChannelService) { }
+
+  ngOnInit(): void {
+    this.refreshData();
+  }
+
+  refreshData() {
+		this.channelService.apiChannelChannelIdGet(this.channelId).subscribe(vm => (this.activeRevision = vm.activeRevision));
+	}
+}
